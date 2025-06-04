5b52e099288cf00863f8637ad3730c08164824db
Priyanshu Verma <priyanshuverma@outlook.in>
2025-06-04 10:26:14 +0000

    custom neovim configs

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,55 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.none-ls"
+    end,
+  },
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+      }
+      return opts
+    end,
+  },
+  { 'wakatime/vim-wakatime', lazy = false },
+  { 'vyfor/cord.nvim' ,lazy = false },
+}
+
+return plugins
